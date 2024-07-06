be224f055d7917dc6074ef58d7e799ce008b7e1c
RaymanKim <rayman.kimari@strathmore.edu>
2024-07-06 12:02:18 +0300

    updates on database

---
+++ consult_app/database/migrations/2024_07_04_115425_create_doctors_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->string('doctorName');
+            $table->string('doctorEmail')->unique();
+            $table->timestamp('doctorEmail_verified_at')->nullable();
+            $table->string('doctorPassword');
+            $table->string('officeLocation')->nullable();
+            $table->string('officeName')->nullable();
+            $table->string('Specialization')->nullable();
+            $table->rememberToken();
+            $table->string('doctorProfile_photo_path', 2048)->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('doctor_password_reset_tokens', function (Blueprint $table) {
+            $table->string('doctorEmail')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
+
+        Schema::create('doctor_sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('doctors_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('doctor_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('doctors');
+        Schema::dropIfExists('doctor_password_reset_tokens');
+        Schema::dropIfExists('doctor_sessions');
+    }
+};
+
+
+
+
--- consult_app/database/seeders/DatabaseSeeder.php
+++ consult_app/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        //seed random users
         User::factory(10)->create();
 
         // User::factory()->create([
